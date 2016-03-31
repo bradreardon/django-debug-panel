328281fc271b53683cf5085446ccf46bd157f46f
Bekhzod <myrubapa@gmail.com>
2016-03-31 16:43:22 +0500

    Support Django 1.10 version
    https://docs.djangoproject.com/en/1.9/internals/deprecation/
    
    django.conf.urls.patterns() will be removed.

--- debug_panel/urls.py
+++ debug_panel/urls.py
@@ -4,15 +4,22 @@ URLpatterns for the debug panel.
 These should not be loaded explicitly; It is used internally by the
 debug-panel application.
 """
-from .views import debug_data
-
-try:
-    from django.conf.urls import url
-except ImportError:  # django < 1.4
-    from django.conf.urls.defaults import url
+import django
 
 _PREFIX = '__debug__'
+_DEBUG_PANEL_URL = r'^%s/data/(?P<cache_key>\d+\.\d+)/$' % _PREFIX
+_DJANGO_VERSION = float(django.get_version().rsplit('.', 1)[0])
 
-urlpatterns = [
-    url(r'^%s/data/(?P<cache_key>\d+\.\d+)/$' % _PREFIX, debug_data, name='debug_data'),
-]
+if _DJANGO_VERSION > 1.4:
+    from django.conf.urls import url
+    from .views import debug_data
+
+    urlpatterns = [
+        url(_DEBUG_PANEL_URL, debug_data, name='debug_data'),
+    ]
+else:
+    from django.conf.urls.defaults import patterns, url
+
+    urlpatterns = patterns('debug_panel.views',
+        url(_DEBUG_PANEL_URL, 'debug_data', name='debug_data'),
+    )
